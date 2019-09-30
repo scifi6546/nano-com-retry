e157e628090e0928712760147325a4e9fb115c3e
nick <sciufi6546@protonmail.com>
2019-09-30 15:35:10 -0800

    foo

--- mmu.h
+++ mmu.h
@@ -1,10 +1,4 @@
-ram_bus device_select(ram_bus* in){
-
-}
-ram_bus bus_read(ram_bus *in){
-
-}
-
+ram_bus device_select(ram_bus* in);
+ram_bus bus_read(ram_bus *in);
 int counter=0;
-
 ram_bus tick(ram_bus *in);
