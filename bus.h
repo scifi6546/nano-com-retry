fdd699ca4928f457cde2ee1295107ce495e92d5f
nick <sciufi6546@protonmail.com>
2019-09-30 16:26:52 -0800

    added ram bus

--- bus.h
+++ bus.h
@@ -4,13 +4,13 @@
 struct bus{
 	short data;
 	char device_select;
-}
+};
 struct ram_bus{
 	short data_bus;
 	short address_bus;
 	char device_select;
-	bool WRITE_OR_READ;
-}
+	char WRITE_OR_READ;
+};
 #define WRITE 0
 #define READ 1
 #endif
--- main.c
+++ main.c
@@ -3,37 +3,9 @@
 struct bus SYSTEM_BUS={0,0};
 struct ram_bus MEMORY_BUS={0,0,0};
 int main(){
-	while(true){
-		system_bus_tick();		
-		ram_bus_tick();
-		tick_counter++;
+	while(0==0){
+		mmu_tick(&SYSTEM_BUS,&MEMORY_BUS);
 	}
 	return 0;
-}
-int tick_counter=0;
-void system_bus_tick(){
-	if(tick_counter%2==0){
-	//cpu_select_device();
-	//cpu_sys_bus_tick()
-	//device1_sysbus_tick();
-	}else{
-		//cpu_sys_bys_read();
-		//device_sys_bus_read();
-	}
-}
-void ram_bus_tick(){
-	tick(&MEMORY_BUS);
-	if(tick_counter%4==0){
-		//device select
-	}if(tick_counter%4==1){
-		//address write
-		//write WRITE or READ to device select
-		//if write write data
-	}if(tick_counter%4==2){
-		//mmu read from bus stage
-		//mmu write to bus
-	}if(tick_counter%4==3){
-		//device read from bus
-	}
 }
 
