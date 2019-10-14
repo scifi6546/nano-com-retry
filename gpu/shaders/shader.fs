7ca8239c441e18ec68cdfd41139d912c7501f727
nick <sciufi6546@protonmail.com>
2019-10-13 16:19:09 -0800

    vga effect

--- gpu/shaders/shader.fs
+++ gpu/shaders/shader.fs
@@ -3,5 +3,8 @@ uniform sampler2D diffuse;
 in vec2 texcoord0;
 out vec4 color;
 void main(){
-   color=texture(diffuse,texcoord0);
+   float col = sin(texcoord0.x*150.0f)*0.05+0.05;
+   vec4 col_out = vec4(col,col,col,1.0f);
+   //vec4 col_out = vec4(texcoord0.y,texcoord0.y,texcoord0.y,texcoord0.y);
+   color=texture(diffuse,texcoord0)+col_out;
 }
